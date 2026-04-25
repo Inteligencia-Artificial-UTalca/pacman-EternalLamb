6ab659ac9b06815fece22397d1004d82a5cc8ca9
EternalLamb <eternallamb1@gmail.com>
2026-04-24 21:47:35 -0400

    Part 1
    ubicacion del movimiento en el controlador de blinky

--- src/BlinkyController.cpp
+++ src/BlinkyController.cpp
@@ -1,5 +1,5 @@
 #include "BlinkyController.h"
-
+#include <iostream>
 
 BlinkyController::BlinkyController(std::shared_ptr<Character> character):Controller(character){
 }
@@ -10,7 +10,53 @@ BlinkyController::~BlinkyController() {
 
 Move
 BlinkyController::getMove(const GameState& game){
-	return PASS;	
-
 	
+	if(character->getDirection()==PASS && game.getMaze().getGhostStart()[0]){
+		return RIGHT;
+	}
+	
+	int pacmanNode = game.getPacmanPos();
+	auto pacmanCoords = game.getMaze().getNodePos(pacmanNode);
+	
+	
+	int ghostNode = character->getPos();
+	
+	Ghost *ghost = dynamic_cast<Ghost*>(character.get());
+	if(!ghost->isEdible()){
+		int minDist=10000000;
+		Move minMove=character->getDirection();
+		std::vector<Move> moves=game.getMaze().getGhostLegalMoves(character->getPos(),character->getDirection());
+		for(Move m:moves){
+
+			int vecino = game.getMaze().getNeighbour(ghostNode,m);
+			
+			if(vecino<0)continue;
+			auto vecinoCoords = game.getMaze().getNodePos(vecino);
+			vecinoCoords.first-=pacmanCoords.first;
+			vecinoCoords.second-=pacmanCoords.second;
+			int sqDist=vecinoCoords.first*vecinoCoords.first+vecinoCoords.second*vecinoCoords.second;
+			if(sqDist<minDist){
+				minDist=sqDist;
+				minMove=m;
+			}
+		}
+		return minMove;
+	}else{
+		int maxDist=-1;
+		Move maxMove=character->getDirection();
+		std::vector<Move> moves=game.getMaze().getGhostLegalMoves(character->getPos(),character->getDirection());
+		for(Move m:moves){
+			int vecino = game.getMaze().getNeighbour(ghostNode,m);
+			if(vecino<0)continue;
+			auto vecinoCoords = game.getMaze().getNodePos(vecino);
+			vecinoCoords.first-=pacmanCoords.first;
+			vecinoCoords.second-=pacmanCoords.second;
+			int sqDist=vecinoCoords.first*vecinoCoords.first+vecinoCoords.second*vecinoCoords.second;
+			if(sqDist>maxDist){
+				maxDist=sqDist;
+				maxMove=m;
+			}
+		}
+		return maxMove;
+	}
 }
